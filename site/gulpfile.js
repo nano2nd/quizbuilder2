926dac03b3d0710bb81b50119e3062229690dc72
nano2nd <javant@iihs.org>
2016-10-31 20:58:07 -0400

    Full copy for deploy

---
+++ site/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var prefixer = require('gulp-autoprefixer');
+
+gulp.task('less', function() {
+    gulp.src('less/app.less')
+        .pipe(less())
+        .pipe(prefixer('last 2 versions'))
+        .pipe(gulp.dest('wwwroot/css'))
+});
+
+gulp.task('gulp-build', ['less']);
+
+gulp.task('default', ['gulp-build'], function() {
+    gulp.watch('./less/*.less', ['less']);
+})
