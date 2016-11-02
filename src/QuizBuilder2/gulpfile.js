1626420f98d6b25f6a68f859f2e532aec5a1e57c
nano2nd <javant@iihs.org>
2016-11-02 00:51:57 -0400

    Adding a publish

--- src/QuizBuilder2/gulpfile.js
+++ src/QuizBuilder2/gulpfile.js
@@ -2,15 +2,19 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var prefixer = require('gulp-autoprefixer');
 
-gulp.task('less', function() {
+gulp.task('build-less', function() {
     gulp.src('less/app.less')
         .pipe(less())
         .pipe(prefixer('last 2 versions'))
         .pipe(gulp.dest('wwwroot/css'))
 });
 
-gulp.task('gulp-build', ['less']);
+gulp.task('build-system', ['build-less']);
 
-gulp.task('default', ['gulp-build'], function() {
-    gulp.watch('./less/*.less', ['less']);
+gulp.task('default', ['build-system'], function() {
+    gulp.watch('./less/*.less', ['build-less']);
 })
+
+gulp.task('publish', ['build-system'], function() {
+    
+});
